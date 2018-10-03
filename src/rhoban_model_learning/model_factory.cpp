49c246bfcfb0f15dae02a2661fd256abb6b4216d
medrimonia <medrimonia@gmail.com>
2018-10-03 18:24:07 +0200

    Adding new types to the model_factory and fixing json error messages + starting some configurations

--- src/rhoban_model_learning/model_factory.cpp
+++ src/rhoban_model_learning/model_factory.cpp
@@ -3,8 +3,14 @@
 #include "rhoban_model_learning/basic_models/linear_model.h"
 
 #include "rhoban_model_learning/humanoid_models/camera_model.h"
+#include "rhoban_model_learning/humanoid_models/multi_poses_model.h"
+#include "rhoban_model_learning/humanoid_models/poses_optimization_model.h"
 #include "rhoban_model_learning/humanoid_models/rotation_model.h"
 #include "rhoban_model_learning/humanoid_models/vision_noise_model.h"
+
+#include "rhoban_model_learning/tags/aruco_collection.h"
+#include "rhoban_model_learning/tags/aruco_cube.h"
+#include "rhoban_model_learning/tags/tags_sheet.h"
 
 // TODO: integrate back 
 //#include "rhoban_model_learning/humanoid_models/vision_correction_model.h"
@@ -17,10 +23,22 @@ namespace rhoban_model_learning
 {
 
 ModelFactory::ModelFactory() {
+  // Basic models
   registerBuilder("LinearModel", []() { return std::unique_ptr<Model>(new LinearModel); });
+  // Humanoid models
   registerBuilder("CameraModel", []() { return std::unique_ptr<Model>(new CameraModel); });
+  registerBuilder("MultiPosesModel",
+                  []() { return std::unique_ptr<Model>(new MultiPosesModel); });
+  registerBuilder("PosesOptimizationModel",
+                  []() { return std::unique_ptr<Model>(new PosesOptimizationModel); });
   registerBuilder("RotationModel", []() { return std::unique_ptr<Model>(new RotationModel); });
   registerBuilder("VisionNoiseModel", []() { return std::unique_ptr<Model>(new VisionNoiseModel); });
+  // Tags models
+  registerBuilder("ArucoCollection", []() { return std::unique_ptr<Model>(new ArucoCollection); });
+  registerBuilder("ArucoCube", []() { return std::unique_ptr<Model>(new ArucoCube); });
+  registerBuilder("TagsSheet", []() { return std::unique_ptr<Model>(new TagsSheet); });
+
+
 //  registerBuilder("VisionCorrectionModel",
 //                  []() { return std::unique_ptr<Model>(new VisionCorrectionModel); });
 //  registerBuilder("TrajectoryPredictor",
